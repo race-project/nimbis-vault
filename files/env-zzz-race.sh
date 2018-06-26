341833a3b62d56439498a083ab7154f0206ca713
Mats Rynge <rynge@localhost.localdomain>
2018-06-26 16:28:05 -0700

    Imlemented Lmod

--- files/env-zzz-race.sh
+++
@@ -1,4 +0,0 @@
-
-PATH=/opt/ngspice/28/bin:/opt/anaconda/bin:$PATH
-export PATH
-
---
+++ files/modulefiles/Core/anaconda.lua
@@ -0,0 +1,9 @@
+help([[
+For detailed instructions, go to:
+   https://conda.io/docs/
+]])
+whatis("URL: https://conda.io/docs/")
+whatis("Description: Package, dependency and environment management for any language—Python, R, Ruby, Lua, Scala, Java, JavaScript, C/ C++, FORTRAN")
+
+prepend_path( "PATH",           "/opt/anaconda/bin")
+
