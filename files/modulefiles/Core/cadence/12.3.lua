a35e691c30fc83a7abef11f6b64567ff00f0a899
Mats Rynge <mats@rynge.net>
2018-07-05 16:16:56 -0700

    Fixed Cadence env

--- files/modulefiles/Core/cadence/12.3.lua
+++ files/modulefiles/Core/cadence/12.3.lua
@@ -6,5 +6,25 @@ whatis("Version: 12.3")
 whatis("URL: https://www.cadence.com/")
 whatis("Cadence is a leading EDA and System Design Enablement provider delivering tools, software, and IP")
 
-prepend_path( "PATH",           "/opt/cadence/12.3/bin")
+local version = "12.3"
+
+setenv("LM_LICENSE_FILE", "1800@license.usc.edu")
+
+setenv("SPECTRE_DEFAULTS", "-E")
+setenv("CDS_Netlisting_Mode", "Analog")
+
+# setup virtuoso
+setenv("CDS_INST_DIR", "/opt/cadence/12.3/ICADV123")
+setenv("MMSIM_HOME", "/opt/cadence/12.3/MMSIM151")
+setenv("CDSHOME", "/opt/cadence/12.3/ICADV123")
+setenv("PVSHOME", "/opt/cadence/12.3/PVS151")
+setenv("OA_HOME", "/opt/cadence/12.3/ICADV123/oa_v22.50.089")
+
+# PATH setup
+prepend_path("PATH", "/opt/cadence/12.3/MMSIM151/tools/bin")
+prepend_path("PATH", "/opt/cadence/12.3/ICADV123/tools/plot/bin")
+prepend_path("PATH", "/opt/cadence/12.3/ICADV123/tools/dfII/bin")
+prepend_path("PATH", "/opt/cadence/12.3/ICADV123/tools/bin")
+prepend_path("PATH", "/opt/cadence/12.3/PVS151/tools/bin")
+
 
