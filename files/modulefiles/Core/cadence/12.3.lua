9586c682f6a16e80b3ad0c04d9e10c9f109d2b5d
Mats Rynge <mats@rynge.net>
2018-07-05 15:18:01 -0700

    Added Cadence

---
+++ files/modulefiles/Core/cadence/12.3.lua
@@ -0,0 +1,10 @@
+help([[
+For detailed instructions, go to:
+   https://www.cadence.com/
+]])
+whatis("Version: 12.3")
+whatis("URL: https://www.cadence.com/")
+whatis("Cadence is a leading EDA and System Design Enablement provider delivering tools, software, and IP")
+
+prepend_path( "PATH",           "/opt/cadence/12.3/bin")
+
