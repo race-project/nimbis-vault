846a528bef6cac99dceb036e8d41e3ff2270c7e3
Karan Vahi <vahi@isi.edu>
2019-04-12 15:34:19 -0700

    Added an ansible role for adonis. It currently only sets env variable CDSHOME to /shared/software/cadence/ICADV123

---
+++ v002/roles/adonis/files/modulefiles/Core/adonis.lua
@@ -0,0 +1,11 @@
+help([[
+For detailed instructions, go to:
+   https://www.cadence.com/
+]])
+whatis("Version: 1.0")
+whatis("URL: https://silicontechnologiesinc.com")
+whatis("Adonis is a flow supported by Silicon Technologies Inc as part of the DARPA CRAFT program")
+
+load("cadence")
+
+setenv("CDSHOME", "/shared/software/cadence/ICADV123")
