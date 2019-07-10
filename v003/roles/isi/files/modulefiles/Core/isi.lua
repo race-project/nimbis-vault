42ddac41cdb2acab7b792e786b8602e3965de320
Rafael Ferreira da Silva <rafsilva@isi.edu>
2019-07-09 22:44:28 -0700

    re-organizing ansible roles

---
+++ v003/roles/isi/files/modulefiles/Core/isi.lua
@@ -0,0 +1,10 @@
+help([[
+ISI Example Flow
+]])
+whatis("ISI Example Flow")
+
+load("anaconda")
+load("cadence_iscape")
+load("cadence_incisive")
+load("cadence_innovus")
+load("cadence_confrml")
--- v003/roles/isi/files/modulefiles/isi.lua
+++
@@ -1,7 +0,0 @@
-help([[
-ISI Example Flow
-]])
-whatis("ISI Example Flow")
-
-load("anaconda")
-load("cadence")
