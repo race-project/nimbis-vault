a9a86ad900a67e99cdc74e5674c1fa3e191adaef
Adam Brinckman <abrinckm@gmail.com>
2020-07-29 12:53:24 -0400

    add script for first time boot on aladdin
    Signed-off-by: Adam Brinckman <abrinckm@gmail.com>

---
+++ ansible/roles/aladdin-vault-app/files/first_boot.sh
@@ -0,0 +1,24 @@
+    cd /usr/lib/aladdin-vault-api
+    
+    export ETH_VM_IP=$(cat /tmp/ethereum_ip.txt || sed 's/ *$//g')
+    export KEY_FILENAME=$(curl $ETH_VM_IP/chaindata/keystore/ | grep -Po '(UTC[^"]*)')
+    curl $ETH_VM_IP/chaindata/keystore/$KEY_FILENAME > keyfile.json
+    echo "RACECRAFT" | node utils/unlock.js `cat ./keyfile.json` &> tmpkey
+    export TMPKEY=`cat tmpkey | grep -oP "0x[0-9a-zA-Z]+" | tail -n 1`
+    export ETHERBASE=$(curl $ETH_VM_IP/priveth/account |grep -oP "0x[0-9a-zA-Z]+")
+
+    sed -i "s/0x0000000000000000000000000000000000000000000000000000000000000000/$TMPKEY/" ./config/local.json
+    sed -i "s/0x0000000000000000000000000000000000000000/$ETHERBASE/" ./config/local.json
+
+    sed -i "s/0x0000000000000000000000000000000000000000000000000000000000000000/$TMPKEY/" /usr/lib/aladdin-vault-cli/.config.json
+    sed -i "s/0x0000000000000000000000000000000000000000/$ETHERBASE/" /usr/lib/aladdin-vault-cli/.config.json
+
+    node utils/deploy.js
+
+    export CONTRACT_ADDRESS=`cat config/local.json | grep "contract_address" | grep -oP "0x[0-9a-zA-Z]+"`
+    export CONTRACT_BLOCK=`cat config/local.json | grep "contract_block" | grep -oP "0x[0-9a-zA-Z]+"`
+
+    sed -i "s/<contract_address>/$CONTRACT_ADDRESS/" /usr/lib/aladdin-vault-cli/.config.json
+    sed -i "s/<contract_block>/$CONTRACT_BLOCK/" /usr/lib/aladdin-vault-cli/.config.json
+    sed -i "s/\"publicKey\": \"\"/\"publicKey\": \"$ETHERBASE\"/" /usr/lib/aladdin-vault-cli/.config.json
+    sed -i "s/\"privateKey\": \"\"/\"privateKey\": \"$TMPKEY\"/" /usr/lib/aladdin-vault-cli/.config.json
